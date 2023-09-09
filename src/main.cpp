d33d49d201c4556481ab03f9e630083eaf635f1e
Sascha Gärtner <sascha.gaertner@hdi.de>
2023-09-09 18:16:36 +0200

    fixing example and test code

--- src/main.cpp
+++ src/main.cpp
@@ -4,13 +4,13 @@
 esp32config::Configuration config("Example Config",{
 	new esp32config::Namespace("First namespace", "ns1", {
 		new esp32config::Entry("Text", esp32config::TEXT, "text", false, "Default text"),
-		new esp32config::Entry("Textarea", esp32config::TEXTAREA, "multiline\ntext", false, "Default\ntext"),
+		new esp32config::Entry("Textarea", esp32config::TEXTAREA, "multiline", false, "Default\ntext"),
 		new esp32config::Entry("Secret", esp32config::PASSWORD, "secret", true, "Default secret"),
 		new esp32config::Entry("Integer", esp32config::INTEGER, "num", false, "123")
 	}),
     new esp32config::Namespace("Second namespace", "ns2", {
 		new esp32config::Entry("Text", esp32config::TEXT, "text"),
-		new esp32config::Entry("Textarea", esp32config::TEXTAREA, "multiline\ntext"),
+		new esp32config::Entry("Textarea", esp32config::TEXTAREA, "multiline"),
 		new esp32config::Entry("Secret", esp32config::PASSWORD, "secret"),
 		new esp32config::Entry("Integer", esp32config::INTEGER, "num")
 	})}
